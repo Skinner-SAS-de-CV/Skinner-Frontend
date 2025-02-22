91c9342768e6f1cab59e240cc7352511c1e7b5a3
Chinaskidev <chinaskicharles0@gmail.com>
2025-02-22 16:01:52 -0600

    api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://fastapi-resume-analyzer-production.up.railway.app";
 
 interface AddJobParams {
   nombre_del_cliente: string;
