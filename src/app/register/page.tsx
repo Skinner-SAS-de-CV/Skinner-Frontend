713e7ba4eb4ab71925e1d9747e0be9e453a35364
William Lemus <william.lemus@gmail.com>
2025-03-24 13:39:44 -0400

    Usar un solo componente(PageContainer) para definir el layout

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,17 +1,11 @@
-import { Navbar } from "@/components/navbar"
-import { Footer } from "@/components/footer"
+import PageContainer from "@/components/pageContainer"
 import RegisterJobForm from "../register/RegisterJobForm"
 
 export default function AnalyzePage() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Navbar />
-      <main className="bg-gray-800 p-6">
-        <RegisterJobForm />
-      </main>
-      
-      <Footer />
-    </div>
+    <PageContainer>
+      <RegisterJobForm />
+    </PageContainer>
   )
 }
 
