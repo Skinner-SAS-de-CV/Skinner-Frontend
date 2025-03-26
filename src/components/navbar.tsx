39cafa84044d041b96b9ed315d0ac68a6228e3c6
William Lemus <william.lemus@gmail.com>
2025-03-25 22:28:06 -0400

    issue-10 remover el botón en las versiones moviles

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,7 @@
-"use client"
+"use client";
 
 import Link from "next/link";
 import { useState } from "react";
-import { Button } from "@/components/ui/button";
 import Logo from "@/components/logo";
 import { Menu, X } from "lucide-react";
 
@@ -20,17 +19,26 @@ export function Navbar() {
 
         {/* Menú para escritorio: enlaces y botón juntos los puse en el mismo contenedor*/}
         <div className="hidden sm:flex items-center space-x-6">
-          <Link href="/" className="text-lg text-gray-300 hover:text-white transition">
+          <Link
+            href="/"
+            className="text-lg text-gray-300 hover:text-white transition"
+          >
             Home
           </Link>
-          <Link href="/about" className="text-lg text-gray-300 hover:text-white transition">
+          <Link
+            href="/about"
+            className="text-lg text-gray-300 hover:text-white transition"
+          >
             Quiénes Somos
           </Link>
         </div>
 
         {/* Botón de menú para móviles estilo hamburguesa */}
         <div className="sm:hidden">
-          <button onClick={toggleMenu} className="text-lg text-gray-300 hover:text-white transition">
+          <button
+            onClick={toggleMenu}
+            className="text-lg text-gray-300 hover:text-white transition"
+          >
             {isOpen ? <X size={24} /> : <Menu size={24} />}
           </button>
         </div>
@@ -39,16 +47,19 @@ export function Navbar() {
       {/* Menú desplegable para móviles */}
       {isOpen && (
         <div className="sm:hidden px-8 pt-4 pb-6 space-y-4">
-          <Link href="/" onClick={() => setIsOpen(false)} className="block text-lg text-gray-300 hover:text-white transition">
+          <Link
+            href="/"
+            onClick={() => setIsOpen(false)}
+            className="block text-lg text-gray-300 hover:text-white transition"
+          >
             Home
           </Link>
-          <Link href="/about" onClick={() => setIsOpen(false)} className="block text-lg text-gray-300 hover:text-white transition">
+          <Link
+            href="/about"
+            onClick={() => setIsOpen(false)}
+            className="block text-lg text-gray-300 hover:text-white transition"
+          >
             Quiénes Somos
-          </Link>
-          <Link href="/analyze" onClick={() => setIsOpen(false)}>
-            <Button className="w-full px-6 py-2 text-white bg-blue-600 hover:bg-blue-700 rounded-lg shadow-lg">
-              App
-            </Button>
           </Link>
         </div>
       )}
