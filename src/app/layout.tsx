5e61ebfc44b1bd774ef41290d45ea052973b6a6f
William Lemus <william.lemus@gmail.com>
2025-04-07 21:28:46 -0400

    issue-16: agregando nombre de llaves para clerk, linting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
-import { ClerkProvider, SignedIn, UserButton } from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
