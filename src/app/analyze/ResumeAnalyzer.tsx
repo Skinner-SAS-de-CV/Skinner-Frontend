a25dc3163e1c502f87b131c8a3bda786b49ea47e
William Lemus <william.lemus@gmail.com>
2025-04-11 01:01:35 -0400

    issue-24: haciendo tres formularios de la manera más simple.

--- src/app/analyze/ResumeAnalyzer.tsx
+++ src/app/analyze/ResumeAnalyzer.tsx
@@ -1,10 +1,17 @@
-
 import AnalyzeForm from "./components/AnalyzeForm";
 
 export default function ResumeAnalyzer({ token }: { token: string | null }) {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-950 md:px-6 py-6">
-      <AnalyzeForm token={token} />
+    <div className="grid grid-rows-3 lg:grid-rows-2 xl:grid-rows-1 grid-flow-col gap-4 items-center justify-center min-h-screen bg-gray-950 md:px-6 py-6">
+      <div className="lg:col-start-1">
+        <AnalyzeForm token={token} />
+      </div>
+      <div className="lg:col-start-2">
+        <AnalyzeForm token={token} />
+      </div>
+      <div className="xl:col-start-3">
+        <AnalyzeForm token={token} />
+      </div>
     </div>
   );
 }
