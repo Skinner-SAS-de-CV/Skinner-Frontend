9e798099084996df7e0f2fd1e9c1b5f5ae128ef8
William Lemus <william.lemus@gmail.com>
2025-04-24 19:07:02 -0400

    issue-38: usar useAuth() en vez de auth()

--- src/app/analyze/ResumeAnalyzer.tsx
+++ src/app/analyze/ResumeAnalyzer.tsx
@@ -1,16 +1,16 @@
 import AnalyzeForm from "./components/AnalyzeForm";
 
-export default function ResumeAnalyzer({ token }: { token: string | null }) {
+export default function ResumeAnalyzer() {
   return (
     <div className="grid grid-rows-3 lg:grid-rows-2 xl:grid-rows-1 grid-flow-col gap-4 justify-center min-h-screen bg-gray-950 md:px-6 py-6">
       <div className="lg:col-start-1">
-        <AnalyzeForm token={token} />
+        <AnalyzeForm />
       </div>
       <div className="lg:col-start-2">
-        <AnalyzeForm token={token} />
+        <AnalyzeForm />
       </div>
       <div className="xl:col-start-3">
-        <AnalyzeForm token={token} />
+        <AnalyzeForm />
       </div>
     </div>
   );
--- src/app/analyze/page.tsx
+++ src/app/analyze/page.tsx
@@ -1,13 +1,10 @@
-import { auth } from "@clerk/nextjs/server";
 import ResumeAnalyzer from "../analyze/ResumeAnalyzer"
 import PageContainer from "@/components/pageContainer"
 
 export default async function AnalyzePage() {
-  const { getToken } = await auth();
-  const token = await getToken();
   return (
     <PageContainer>
-      <ResumeAnalyzer token={token} />
+      <ResumeAnalyzer />
     </PageContainer>
   )
 }
