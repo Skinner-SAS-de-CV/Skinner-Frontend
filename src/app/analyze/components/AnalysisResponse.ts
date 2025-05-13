f8a7fbebfd4d7318f955ceadc58aeb70a9e3d8ce
William Lemus <william.lemus@gmail.com>
2025-05-13 13:50:29 -0400

    issue-40: modificando nombre de archivo para que use nombre de candidato

--- src/app/analyze/components/AnalysisResponse.ts
+++ src/app/analyze/components/AnalysisResponse.ts
@@ -2,6 +2,7 @@ export type AnalysisResponse = {
   file_name: string;
   match_score: number;
   decision: string; // talvez es un enum?
+  nombre_de_candidato: string;
   feedback: {
     feedback: string;
   };
