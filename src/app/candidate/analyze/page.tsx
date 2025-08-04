42948aa43eec05d1f4e15a491a97a3d5f219d75d
William Lemus <william.lemus@gmail.com>
2025-08-03 20:06:49 -0400

    issue-77: agregando mensaje de saldo restante

--- src/app/candidate/analyze/page.tsx
+++ src/app/candidate/analyze/page.tsx
@@ -1,12 +1,21 @@
 import PageContainer from "@/components/pageContainer";
 import AnalyzeForm from "./components/AnalyzeForm";
+import { getSaldo } from "@/lib/api/candidato";
+import { auth } from "@clerk/nextjs/server";
 
-export default function Analyze() {
+export default async function Analyze() {
+  // Obtener el saldo del usuario y mostrarlo en el componente AnalyzeForm
+  const { userId, getToken } = await auth();
+  let saldo = 0;
+  const token = await getToken();
+  if (userId && token) {
+    saldo = await getSaldo(userId, token);
+  }
   return (
     <PageContainer>
       <div className="min-h-screen bg-gray-950 md:px-6 py-6">
         {/* Cambiále el nombre a este componente */}
-        <AnalyzeForm />
+        <AnalyzeForm saldo={saldo} />
       </div>
     </PageContainer>
   );
