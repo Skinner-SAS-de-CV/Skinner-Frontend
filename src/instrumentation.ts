4b8dc8eed8a87660646a0b09f1786c08b534eae2
Chinaskidev <chinaskicharles0@gmail.com>
2025-10-25 13:57:19 -0300

    actulaizando el sentry del front

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -6,7 +6,7 @@ export async function register() {
   }
 
   if (process.env.NEXT_RUNTIME === 'edge') {
-    await import('../sentry.edge.config');
+    console.log ("Estoy en el edge maje")
   }
 }
 
