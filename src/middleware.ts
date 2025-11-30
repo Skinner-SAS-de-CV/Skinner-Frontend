1c421e096bba25442a84a4e6ea857e257ae83c60
Chinaskidev <chinaskicharles0@gmail.com>
2025-11-29 21:15:50 -0300

    middleware poniendolo en true

--- src/middleware.ts
+++ src/middleware.ts
@@ -62,7 +62,7 @@ export default clerkMiddleware(
       }
     }
   },
-  { debug: process.env.NODE_ENV === "production" ? false : false}
+  { debug: process.env.NODE_ENV === "production" ? false : true}
 );
 
 export const config = {
